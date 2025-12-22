5597bf62ebd898ab2098faac303be3d873f47ed1
shottyman <Jyssauco1@gmail.com>
2025-12-22 18:17:49 -0500

    Continued to Textures Example

---
+++ resources/shader.fs
@@ -0,0 +1,10 @@
+#version 460 core
+out vec4 FragColor;
+in vec3 ourColor;
+in vec2 TexCoord;
+uniform sampler2D ourTexture;
+uniform sampler2D ourTexture1;
+void main()
+{
+  FragColor = mix(texture(ourTexture, TexCoord), texture(ourTexture1, TexCoord), 0.2);
+}
