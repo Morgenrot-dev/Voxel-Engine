5e72a6350dca23bae583d2e521647a3ad03bc900
shottyman <Jyssauco1@gmail.com>
2025-12-23 14:39:15 -0500

    Added camera positions and directions

--- src/editor.cxx
+++ src/editor.cxx
@@ -64,6 +64,13 @@ int main()
 
   glm::mat4 projection;
   projection = glm::perspective(glm::radians(45.0f), 640.0f/480.0f , 0.1f , 100.0f);
+  
+  glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+  glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+  glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+  glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+  glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 
   float vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
